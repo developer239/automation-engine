068524d1cfed461c311c2750e91db046db144d39
michaljarnot <m.jarnot@yahoo.com>
2022-12-21 16:23:18 +0100

    feat: make basic CMake, SDL, Imgui program work

--- main.cpp
+++ main.cpp
@@ -1,6 +1,64 @@
-#include <iostream>
+#include <SDL.h>
+
+#include "externals/imgui/backends/imgui_impl_sdl.h"
+#include "externals/imgui/backends/imgui_impl_sdlrenderer.h"
+#include "externals/imgui/imgui.h"
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+  SDL_Window* window;
+
+  SDL_Init(SDL_INIT_EVERYTHING);
+
+  SDL_DisplayMode displayMode;
+  SDL_GetCurrentDisplayMode(0, &displayMode);
+
+  window = SDL_CreateWindow(
+      nullptr,
+      SDL_WINDOWPOS_CENTERED,
+      SDL_WINDOWPOS_CENTERED,
+      800,
+      600,
+      0
+  );
+
+  SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO();
+
+  ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
+  ImGui_ImplSDLRenderer_Init(renderer);
+
+  bool shouldQuit = false;
+  SDL_Event event;
+
+  while (!shouldQuit) {
+    while (SDL_PollEvent(&event)) {
+      ImGui_ImplSDL2_ProcessEvent(&event);
+
+      if (event.type == SDL_QUIT) {
+        shouldQuit = true;
+      }
+    }
+
+    ImGui_ImplSDLRenderer_NewFrame();
+    ImGui_ImplSDL2_NewFrame();
+    ImGui::NewFrame();
+    ImGui::ShowDemoWindow();
+    ImGui::Render();
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 100, 0);
+    SDL_RenderClear(renderer);
+
+    ImGui_ImplSDLRenderer_RenderDrawData(ImGui::GetDrawData());
+    SDL_RenderPresent(renderer);
+  }
+
+  ImGui_ImplSDLRenderer_Shutdown();
+  ImGui_ImplSDL2_Shutdown();
+  ImGui::DestroyContext();
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
 }
