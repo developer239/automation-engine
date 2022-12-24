93cc2b4d447032cc059cc49561d876949fafaf04
michaljarnot <m.jarnot@yahoo.com>
2022-12-24 14:56:40 +0100

    fix: simplify delay test

--- src/packages/utility/utility/Delay.test.cpp
+++ src/packages/utility/utility/Delay.test.cpp
@@ -2,24 +2,13 @@
 #include <gmock/gmock.h>
 #include <chrono>
 
-#include "Delay.h"
+#include "./Delay.h"
 
 using ::testing::Return;
 
-class DelayMock {
-  public:
-    MOCK_METHOD(void, delay, (unsigned int), ());
-};
-
 TEST(DelayTest, TestDelay) {
-  DelayMock mock;
-
-  ON_CALL(mock, delay(1000)).WillByDefault(::testing::Invoke([]() {
-    delay(1000);
-  }));
-
   auto start = std::chrono::high_resolution_clock::now();
-  mock.delay(1000);
+  delay(1000);
   auto end = std::chrono::high_resolution_clock::now();
 
   auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
