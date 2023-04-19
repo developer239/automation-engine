fe50962ae782c085ff302ca73d6d1505211b8493
michaljarnot <m.jarnot@yahoo.com>
2023-04-19 21:17:16 +0200

    feat: make minimal onnx work

--- README.md
+++ README.md
@@ -6,12 +6,14 @@ It is goal is to provide a simple way to automate tasks on a computer by writing
 
 ### Requirements:
 
+- `$ brew install pkg-config`
 - `$ brew install cmake`
 - `$ brew install opencv`
 - `$ brew install sdl2`
 - `$ brew install sdl2_ttf`
 - `$ brew install sdl2_image`
 - `$ brew install sdl2_mixer`
+- `$ brew install onnxruntime`
 
 ### Packages
 
--- src/apps/automation-engine/src/main.cpp
+++ src/apps/automation-engine/src/main.cpp
@@ -2,8 +2,16 @@
 #include "core/Loop.h"
 
 #include "./strategies/ECSStrategy.h"
+#include "services/yolo/YOLOSegmentor.h"
 
 int main() {
+  SegNetConfig DetectorConfig = {
+      0.3,
+      0.3,
+      "../../../../models/second-best-n-640-seg.onnx",
+      "../../../../models/class.names"};
+  YOLOSegmentor net(DetectorConfig);
+
   CoreImGui::Strategy gui;
   ECSStrategy ecs;
 
