b1f11aff00b3cbc3fb32f2cd0e70929363edb100
michaljarnot <m.jarnot@yahoo.com>
2023-04-20 12:42:29 +0200

    feat: create entities for detected segments

--- src/apps/automation-engine/src/systems/Detection/InstanceSegmentationSystem.h
+++ src/apps/automation-engine/src/systems/Detection/InstanceSegmentationSystem.h
@@ -19,17 +19,27 @@ class InstanceSegmentationSystem : public DetectionSystemBase {
     for (auto entity : GetSystemEntities()) {
       auto screenshotDebug = screen->latestScreenshot.clone();
       ApplyOperations(entity, screenshotDebug, screen);
-      DetectObjects(entity, screen->latestScreenshot, screen);
+      InstanceSegmentation(entity, screen->latestScreenshot, screen);
     }
   }
 
  private:
-  void DetectObjects(
+  // TODO: sometimes crashes the app
+  // libc++abi: terminating with uncaught exception of type cv::Exception:
+  // OpenCV(4.7.0)
+  // /tmp/opencv-20230122-99400-19ne6ip/opencv-4.7.0/modules/core/src/matrix.cpp:809:
+  // error: (-215:Assertion failed) 0 <= roi.x && 0 <= roi.width && roi.x +
+  // roi.width <= m.cols && 0 <= roi.y && 0 <= roi.height && roi.y + roi.height
+  // <= m.rows in function 'Mat'
+  void InstanceSegmentation(
       ECS::Entity& entity, cv::Mat& screenshotDebug,
       std::optional<Devices::Screen>& screen
   ) {
-    auto [imageInstanceSegmentationComponent, group] =
-        GetComponentAndGroup<InstanceSegmentationComponent>(entity, "segment-image");
+    auto [instanceSegmentationComponent, group] =
+        GetComponentAndGroup<InstanceSegmentationComponent>(
+            entity,
+            "segment-image"
+        );
 
     KillComponentsInGroup(group);
 
@@ -38,7 +48,25 @@ class InstanceSegmentationSystem : public DetectionSystemBase {
     //
     // Detection logic START
 
-    imageInstanceSegmentationComponent.net->Detect(screenshotDebug);
+    auto segments = instanceSegmentationComponent.net->Detect(screenshotDebug);
+
+    for (auto segment : segments) {
+      auto match = ECS::Registry::Instance().CreateEntity();
+      ECS::Registry::Instance().GroupEntity(match, group);
+
+      int targetX = segment.bbox.x + offset.x;
+      int targetY = segment.bbox.y + offset.y;
+
+      auto randomColor = App::Color({rand() % 255, rand() % 255, rand() % 255});
+
+      ECS::Registry::Instance().AddComponent<BoundingBoxComponent>(
+          match,
+          App::Position({targetX, targetY}),
+          App::Size({segment.bbox.width, segment.bbox.height}),
+          randomColor,
+          3
+      );
+    }
 
     // Detection logic END
     //
